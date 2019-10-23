3597d324a1a0ea382308b778586652b7bd7b48a6
Flavio Miranda <vinniimiranda@gmail.com>
2019-10-23 09:53:55 -0300

    Heroku setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+const serveStatic = require('serve-static');
+
+const port = process.env.PORT || 8080;
+
+app.use(serveStatic(path.join(__dirname, '/build/')));
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/dist/index.html'));
+
+app.listen(port);
+console.log('Servidor rodando na porta: ' + port);
