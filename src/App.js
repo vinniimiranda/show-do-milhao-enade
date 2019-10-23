05a366e00b2cb44a210c1f4ddd94fbb5800f5ad9
vinniimiranda <vinniimiranda@gmail.com>
2019-10-22 23:51:41 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
+  const [respostas, setRespostas] = useState([
+    {
+      option: "A",
+      value: "Encapsulamento",
+      correct: true
+    },
+    {
+      option: "B",
+      value: "Atributo Privado",
+      correct: false
+    },
+    {
+      option: "C",
+      value: "Atributo Protegido",
+      correct: false
+    },
+    {
+      option: "D",
+      value: "Atributo Abstrato",
+      correct: false
+    },
+    {
+      option: "E",
+      value: "Herança",
+      correct: false
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="question-card">
+        <p className="question">
+          Na programação como é chamado o conceito que protege um atributo no
+          modelo de Classes?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="answer-card">
+        {respostas.map(resposta => (
+          <div className="answer" key={resposta.option}>
+            <div
+              className="option"
+              onClick={() => {
+                if (resposta.correct) {
+                  alert("Resposta Correta");
+                } else {
+                  alert("Resposta Errada!");
+                }
+              }}
+            >
+              <span className="number">{resposta.option}</span>
+              <span className="text"> {resposta.value}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
