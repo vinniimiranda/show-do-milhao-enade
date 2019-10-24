a5128b02e83833d7a4ff379b1d2085e9c9d78797
vinniimiranda <vinniimiranda@gmail.com>
2019-10-23 23:23:38 -0300

    Grid system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
 import "react-toastify/dist/ReactToastify.css";
 
 import logo from "./logo.png";
@@ -38,38 +41,59 @@ function App() {
 
   return (
     <div className="App">
-      <ToastContainer
-        enableMultiContainer
-        position={toast.POSITION.TOP_RIGHT}
-      />
-      <div className="logo">
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-      <div className="question-card">
-        <p className="question">
-          Na programação como é chamado o conceito que protege um atributo no
-          modelo de Classes?
-        </p>
-      </div>
-      <div className="answer-card">
-        {respostas.map(resposta => (
-          <div className="answer" key={resposta.option}>
-            <div
-              className="option"
-              onClick={() => {
-                if (resposta.correct) {
-                  notifySucces();
-                } else {
-                  notifyError();
-                }
-              }}
-            >
-              <span className="number">{resposta.option}</span>
-              <span className="text"> {resposta.value}</span>
+      <Container maxWidth="sm">
+        <ToastContainer
+          enableMultiContainer
+          position={toast.POSITION.TOP_RIGHT}
+        />
+        <Grid container spacing={2} className="container">
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+            
+          </Grid>
+          <Grid item xs={12}>
+            <div className="question-card">
+              <p className="question">
+                Na programação como é chamado o conceito que protege um atributo
+                no modelo de Classes?
+              </p>
             </div>
-          </div>
-        ))}
-      </div>
+          </Grid>
+          <Grid item xs={12}>
+            {respostas.map(resposta => (
+              <div className="answer" key={resposta.option}>
+                <div
+                  className="option"
+                  onClick={() => {
+                    if (resposta.correct) {
+                      notifySucces();
+                    } else {
+                      notifyError();
+                    }
+                  }}
+                >
+                  <span className="number">{resposta.option}</span>
+                  <span className="text"> {resposta.value}</span>
+                </div>
+              </div>
+            ))}
+          </Grid>
+          <Grid item xs={4} className="text-center ">
+              <div className="info">
+                <h5>Errar</h5>
+              </div>
+          </Grid>
+          <Grid item xs={4} className="text-center ">
+              <div className="info">
+                <h5>Acertar</h5>
+              </div>
+          </Grid>
+          <Grid item xs={4} className="text-center ">
+             <div className="info">
+                <h5>Parar</h5>
+              </div>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
